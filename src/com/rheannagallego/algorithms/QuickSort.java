c2b60496860a9a7d03f1c70f8339ded38a82999f
Rheanna <gallego.rheanna@gmail.com>
2020-06-11 13:58:27 -0700

    Fix QuickSort animation. Closes #5.

--- src/com/rheannagallego/algorithms/QuickSort.java
+++ src/com/rheannagallego/algorithms/QuickSort.java
@@ -2,7 +2,7 @@ package com.rheannagallego.algorithms;
 
 public class QuickSort {
 
-    public void quicksort(int[] arr, int start, int end) {
+    public static void quicksort(int[] arr, int start, int end) {
         if (end - start < 2) {
             return;
         }
@@ -12,7 +12,7 @@ public class QuickSort {
         quicksort(arr, partitionIdx + 1, end);
     }
 
-    private int partition(int[] arr, int start, int end) {
+    private static int partition(int[] arr, int start, int end) {
         int pivot = arr[start];
         int i = start;
         int j = end;
@@ -20,13 +20,28 @@ public class QuickSort {
             while (i < j && arr[--j] >= pivot ) { continue; }
             if (i < j) {
                 arr[i] = arr[j];
+                printArr(arr);
             }
             while (i < j && arr[++i] <= pivot) { continue; }
             if (i < j) {
                 arr[j] = arr[i];
+                printArr(arr);
             }
         }
         arr[i] = pivot;
+        printArr(arr);
         return i;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {20, 35, 5, 10, 40, 8, 2};
+        quicksort(arr, 0, 7);
+    }
+
+    private static void printArr(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
--- src/com/rheannagallego/algorithms/QuickSortAnimation.java
+++ src/com/rheannagallego/algorithms/QuickSortAnimation.java
@@ -1,17 +1,19 @@
 package com.rheannagallego.algorithms;
 
 import com.rheannagallego.view.MainWindow;
-import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
-import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuickSortAnimation extends AlgorithmAnimation {
+    List<Boolean> isPivot = new ArrayList<>();
 
     @Override
     public void startSort(int[] arr) {
+        initializeSPOrder();
         sort(arr, 0, arr.length);
     }
 
@@ -27,58 +29,57 @@ public class QuickSortAnimation extends AlgorithmAnimation {
 
     private int partition(int[] arr, int start, int end) {
 
-        int pivot = arr[start];
+        int pivot = arr[start]; int startIdx = spOrder[start];
         int i = start;
         int j = end;
         while (i < j) {
             while (i < j && arr[--j] >= pivot ) { continue; }
             if (i < j) {
                 arr[i] = arr[j];
-                addTransition(i, j);
+                addTransition(i, j, start, false);
+                isPivot.add(false);
             }
             while (i < j && arr[++i] <= pivot) { continue; }
             if (i < j) {
                 arr[j] = arr[i];
-                addTransition(j, i);
+                addTransition(j, i, start, false);
+                isPivot.add(false);
             }
         }
+
         arr[i] = pivot;
-        addTransition(i, start);
+
+        addTransition(i, startIdx, start, true);
+        isPivot.add(true);
+
         return i;
     }
 
-    @Override
-    public void playAnimation(){
+    private void addTransition(int to, int from, int start, boolean pivot){
+        int variance;
+        StackPane movedBar;
 
-        SequentialTransition sq = new SequentialTransition();
-
-        for(int i = 0; i < transitions.size(); i++){
-
-            Integer[] currentTransition = transitions.get(i);
-            int toLoc = currentTransition[0];
-            int fromLoc = currentTransition[1];
-            int value = currentTransition[2];
-            StackPane movedPane = new StackPane();
-            StackPane tmpPane;
-
-            for(Node cell : MainWindow.chartPane.getChildren()){
-                if(cell instanceof StackPane) {
-                    tmpPane = (StackPane) cell;
-                    if(((Text) tmpPane.getChildren().get(1)).getText().equals(Integer.toString(value))){
-                        movedPane = tmpPane;
-                    }
-                }
-
-            }
-
-            TranslateTransition tt = new TranslateTransition(Duration.millis(1000), movedPane);
-
-            tt.setByX(25 * (toLoc - fromLoc));
-            tt.setAutoReverse(false);
-
-            sq.getChildren().add(tt);
+        if(pivot){
+            variance = start - to;
+            movedBar = (StackPane) MainWindow.chartPane.getChildren().get(from);
+            spOrder[to] = from;
+        }else{
+            variance = from - to;
+            movedBar = (StackPane) MainWindow.chartPane.getChildren().get(spOrder[from]);
+            spOrder[to] = spOrder[from];
         }
-        sq.play();
+
+        addTrTransition(-variance, movedBar);
     }
 
+    private void addTrTransition(int variance, StackPane bar){
+        TranslateTransition tt = new TranslateTransition(Duration.millis(1000), bar);
+        tt.setByX(25 * variance);
+        tt.setAutoReverse(false);
+        sq.getChildren().add(tt);
+    }
+
+    public void playAnimation() {
+        playSequentialTransition();
+    }
 }
